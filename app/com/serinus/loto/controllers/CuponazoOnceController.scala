1da42542d7e986f10561e51951ba7d1d9f567b3c
David Santiago <d4v1x.sb@gmail.com>
2017-02-07 21:09:41 +0000

    Added two new actions in the Cuponazo Once controller to compute the most and least frequent combinations based on the frequency number.

--- app/com/serinus/loto/controllers/CuponazoOnceController.scala
+++ app/com/serinus/loto/controllers/CuponazoOnceController.scala
@@ -20,4 +20,29 @@ class CuponazoOnceController @Inject() (cuponazoStats: CuponazoOnceStats,
     }
   }
 
+  def findFrequencies = Action.async {
+    cuponazoStats.computeFrequencies() map {
+      case Right(freqs) => Ok(Json.toJson(freqs))
+      case Left(err) => InternalServerError(s"Error computing the Cuponazo frequencies: $err")
+    }
+  }
+
+
+  def findMostFrequentCombination = Action.async {
+    cuponazoStats.computeMostFrequentCombination() map {
+      case Right(comb) => Ok(Json.toJson(comb))
+      case Left(err) => InternalServerError(s"Error the most frequent Cuponazo combination: $err")
+    }
+
+  }
+
+
+  def findLeastFrequentCombination = Action.async {
+    cuponazoStats.computeLeastFrequentCombination() map {
+      case Right(comb) => Ok(Json.toJson(comb))
+      case Left(err) => InternalServerError(s"Error the least frequent Cuponazo combination: $err")
+    }
+
+  }
+
 }
